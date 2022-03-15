3d620266966a905edd15c30bb7f25ef0d29f37f7
joshua david jennings <jennings.josh@gmail.com>
2022-03-14 23:01:48 -0500

    done

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,27 +1,64 @@
 import React, { useState } from 'react';
 import { Form } from 'semantic-ui-react';
 
-function PokemonForm() {
+function PokemonForm({ addNewPokemon }) {
+	const defaultState = {
+		name: '',
+		hp: '',
+		frontURL: '',
+		backURL: '',
+	};
+
+	const [formData, setFormData] = useState(defaultState);
+
+	const handleChange = (e) => {
+		const thingWeChange = e.target.name;
+		setFormData(() => ({ ...formData, [thingWeChange]: e.target.value }));
+	};
+
 	return (
 		<div>
 			<h3>Add a Pokemon!</h3>
 			<Form
-				onSubmit={() => {
+				onSubmit={(e) => {
+					e.preventDefault();
 					console.log('submitting form...');
+
+					// reset the form
+					setFormData(defaultState);
+					addNewPokemon(formData);
 				}}
 			>
 				<Form.Group widths="equal">
-					<Form.Input fluid label="Name" placeholder="Name" name="name" />
-					<Form.Input fluid label="hp" placeholder="hp" name="hp" />
+					<Form.Input
+						fluid
+						label="Name"
+						value={formData.name}
+						onChange={handleChange}
+						placeholder="Name"
+						name="name"
+					/>
+					<Form.Input
+						fluid
+						label="hp"
+						value={formData.hp}
+						onChange={handleChange}
+						placeholder="hp"
+						name="hp"
+					/>
 					<Form.Input
 						fluid
 						label="Front Image URL"
+						value={formData.frontURL}
+						onChange={handleChange}
 						placeholder="url"
 						name="frontUrl"
 					/>
 					<Form.Input
 						fluid
 						label="Back Image URL"
+						value={formData.backURL}
+						onChange={handleChange}
 						placeholder="url"
 						name="backUrl"
 					/>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -22,11 +22,32 @@ function PokemonPage() {
 		return pokemon.filter((p) => p.name.includes(search.toLowerCase()));
 	};
 
+	const addNewPokemon = (pokemonData) => {
+		const dataToSend = {
+			...pokemonData,
+			sprites: { front: pokemonData.frontUrl, back: pokemonData.backUrl },
+		};
+		delete dataToSend.frontUrl;
+		delete dataToSend.backUrl;
+
+		const configObj = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(dataToSend),
+		};
+
+		fetch('http://localhost:3001/pokemon', configObj)
+			.then((r) => r.json())
+			.then((pokemon) => setPokemon((mUV) => [...mUV, pokemon]));
+	};
+
 	return (
 		<Container>
 			<h1>Pokemon Searcher</h1>
 			<br />
-			<PokemonForm />
+			<PokemonForm addNewPokemon={addNewPokemon} />
 			<br />
 			<Search search={search} handleSearch={handleSearch} />
 			<br />
